a84f3f13e0110253a158b50e06a1556cca827944
harshitMaurya18-cloud <harshitmourya80@gmail.com>
2026-09-06 21:07:21 +0530

    Update server log message to include full address

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB:", DB_PATH);
     app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on address http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
